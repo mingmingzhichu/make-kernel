f8a5f963f47b9976fbd39af98d6858a8a93ca051
lihaoyu <109925622+mingmingzhichu@users.noreply.github.com>
2023-03-05 13:05:23 +0800

    Add files via upload

---
+++ 1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo touch /tmp/keepalive
+cd kernel
+sudo apt install llvm lld clang gcc make binutils flex bison kmod util-linux e2fsprogs jfsutils udev reiserfsprogs xfsprogs bc squashfs-tools btrfs-progs openssl pcmciautils quotatool ppp nfs-common oprofile procps iptables libssl-dev python3-sphinx git python2
+cd 1
+unzip make-3.81.zip
+cd make-3.81
+chmod 777 ./configure
+./configure
+make 
+cd ../../kernel
+git clone https://github.com/EndCredits/kernel_xiaomi_sm7250.git
+cd kernel_xiaomi_sm7250
+chmod 777 build.sh
+./build.sh all
